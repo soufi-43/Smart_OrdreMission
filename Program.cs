fe7ea37db05ff4adb22e21454e22e7557220c69e
soufi-43 <soufi-43@hotmail.fr>
2025-08-06 00:12:14 +0100

    Configure app to use PORT from environment for Render

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
